46e494f9bc21b72f57018a71ed9c14df0b417077
mirelvt <mailenm@mirellavanteulingen.nl>
2015-09-09 07:13:06 +0200

    Add transition between the images, generate the data attributes, add basic styling, add tweenmax lib, update readme.

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# Simple Javsacript, CSS Lightbox with GSAP
+# MVT-Lightbox 1.0
+
+This simple lightbox uses Javascript, CSS and [GSAP](http://greensock.com/gsap).
+For the arrows + close button I use inline svg. You can replace them with your own
+images or icons.
+
+## Dependencies
+
+- mvt-lightbox.min.js
+- tweenmax.min.js
+- lightbox.css
+
+## Usage
+
+Add tweenmax.min.js and mvt-lightbox.min.js in the bottom of the body:
+```html
+<script src="js/tweenmax.min.js" type="text/javascript"></script>
+<script src="js/mvt-lightbox.min.js" type="text/javascript"></script>
+```
+
+Add mvt-lightbox.min.css file to your html page:
+```html
+<link rel="stylesheet" type="text/css" href="lightbox.min.css">
+```
+
+The html structure of the lightbox is as follows:
+```html
+<section data-lightbox="mvt-lightbox">
+    <ul>
+        <li>
+            <img src="[path to your thumbnail image]" alt="">
+        </li>
+        <li>
+            <img src="[path to your thumbnail image]" alt="">
+        </li>
+        <li>
+            <img src="[path to your thumbnail image]" alt="">
+        </li>
+        <li>
+            <img src="[path to your thumbnail image]" alt="">
+        </li>
+        <li>
+            <img src="[path to your thumbnail image]" alt="">
+        </li>
+    </ul>
+    <div class="lightbox" data-show-id="">
+        <div class="img-list">
+            <img src="[path to your image]" alt="">
+            <img src="[path to your image]" alt="">
+            <img src="[path to your image]" alt="">
+            <img src="[path to your image]" alt="">
+            <img src="[path to your image]" alt="">
+        </div>
+        <div class="btn-close">
+            <svg viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg">
+                <title>Close</title>
+                <path d="M27.344,4.287 L23.531,0.474 L14,10.005 L4.469,0.474 L0.656,4.287 L10.187,13.818 L0.656,23.349 L4.469,27.162 L14,17.631 L23.531,27.162 L27.344,23.349 L17.813,13.818 L27.344,4.287 Z"></path>
+            </svg>
+        </div>
+        <div class="lightbox-nav-next">
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <title>Next</title>
+                <path d="M181.683 512.5L96.35 427.203l170.632-170.67L96.352 85.868 181.68.5 437.65 256.534 181.682 512.5z" />
+            </svg>
+        </div>
+        <div class="lightbox-nav-prev">
+            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <title>Previous</title>
+                <path d="M85.683,512.5 L0.35,427.203 L170.982,256.533 L0.352,85.868 L85.68,0.5 L341.65,256.534 L85.682,512.5 L85.683,512.5 Z" transform="translate(171.000000, 256.500000) scale(-1, 1) translate(-171.000000, -256.500000) "/>
+            </svg>
+        </div>
+    </div>
+</section>
+```
+
+To initialize the lightbox, add the following line to your javascript file in DOMContentLoaded:
+```js
+mvtLightBox(document.querySelector('[data-lightbox="mvt-lightbox"]'));
+```
+
+
+
+
+
--- js/mvt-lightbox.js
+++ js/mvt-lightbox.js
@@ -1,60 +1,99 @@
+/*
+ * MVT-Lightbox
+ * https://github.com/mirelvt/mvt-lightbox
+ *
+ * Released under the MIT license
+ *
+ * Date: 2015-09-08
+ */
 (function() {
     'use strict';
     /* global TweenMax:false */
 
-    function lightBox(container) {
-        var thumbs = document.querySelectorAll('li'),
-        lightbox = document.querySelector('.lightbox'),
+    var mvtLightBox = function lightBox(container) {
+        var thumbs = container.querySelectorAll('li'),
+        lightbox = container.querySelector('.lightbox'),
         img_list = lightbox.querySelector('.img-list'),
+        images = img_list.querySelectorAll('img'),
         nav_next = lightbox.querySelector('.lightbox-nav-next'),
         nav_prev = lightbox.querySelector('.lightbox-nav-prev');
 
+        /* ***
+         * Add click events to the thumbnails, close + prev/next elements
+        *** */
         lightbox.querySelector('.btn-close').addEventListener('click', closeLightBox, false);
         nav_next.addEventListener('click', navLightBox, false);
         nav_prev.addEventListener('click', navLightBox, false);
 
-        // Assign click event on all the thumbs
         for (var i = 0; i < thumbs.length; i++) {
             thumbs[i].addEventListener('click', showLightBox, false);
+            // Set data-target attribute
+            thumbs[i].setAttribute('data-target', 'image-' + [i + 1]);
         };
 
-        // The lightbox and image photo is shown based on the value of data-show-id and data-id,
-        // if they match show the lightbox + matched photo
+        // Set data-id attribute on each gallery image
+        for (var i = 0; i < images.length; i++) {
+            images[i].setAttribute('data-id', 'image-' + [i + 1]);
+        };
+
+        /* ***
+         * The lightbox and photo is shown based on the value of data-show-id and data-id,
+         * if they match: show the lightbox + photo.
+        *** */
         function showLightBox(evt) {
             // Update data-show-id attribute
             var target = evt.currentTarget.getAttribute('data-target');
-            lightbox.setAttribute('data-show-id', target);
-            showPhoto(target);
-
             var photo = img_list.querySelector('[data-id="' +  target + '"]');
-            animateLightBox(photo);
+            lightbox.setAttribute('data-show-id', target);
+            animateLightBox(photo, target);
         }
 
         // Start the animation and set the width + height of the lightbox based on the image width/height
-        function animateLightBox(photo) {
+        function animateLightBox(photo, target) {
             TweenMax.to(lightbox, 0.5, {
                 css:{
                     display: "block",
                     opacity: 1,
-                    "max-height": photo.naturalHeight + "px",
-                    "max-width": photo.naturalWidth + "px"
+                    "height": photo.naturalHeight + "px",
+                    "width": photo.naturalWidth + "px"
                 },
                 ease: Power2.easeOut
             });
 
+            togglePhoto(target);
         }
 
         // Show the target photo in the lightbox container
-        function showPhoto(target) {
+        function togglePhoto(target) {
             var img = img_list.querySelector('[data-id="' + target + '"]');
-            var images = img_list.querySelectorAll('img');
 
-            // first hide all images
+            // First animate all images to hidden state
             for (var i = 0; i < images.length; i++) {
-                images[i].style.display = "none";
+                TweenMax.to(images[i], 0.5, {
+                    css:{
+                        display: "none",
+                        opacity: 0,
+                    },
+                    ease: Power2.easeOut
+                });
             }
-            // Than show the selected image
-            img.style.display = "block";
+            // Then show the selected image
+            TweenMax.fromTo(img, 0.5,
+                {
+                    css:{
+                        display: "none",
+                        opacity: 0,
+                    }
+                },
+                {
+                    css:{
+                        display: "block",
+                        opacity: 1,
+                    },
+                    ease: Power2.easeOut
+                }
+            );
+
             // Show the lightbox nav arrows
             toggleLightBoxNav(img);
         }
@@ -74,7 +113,24 @@
 
         // Remove all inline styles from the lightbox to hide it
         function closeLightBox() {
+            TweenMax.to(lightbox, 0.5, {
+                css:{
+                    display: "none",
+                    opacity: 0,
+                    "height": 0,
+                    "width": 0
+                },
+                ease: Power2.easeOut,
+                onComplete: resetStyles
+            })
+        }
+
+        // After the closing animation is done, remove the style attributes
+        function resetStyles() {
             lightbox.removeAttribute('style');
+            for (var i = 0; i < images.length; i++) {
+                images[i].removeAttribute('style');
+            }
         }
 
         // Handle the prev and next click event
@@ -90,13 +146,12 @@
             lightbox.setAttribute('data-show-id', next_elm);
             next_id = lightbox.getAttribute('data-show-id');
             photo = lightbox.querySelector('[data-id="' +  next_id + '"]');
-            animateLightBox(photo);
-            showPhoto(next_id);
+            animateLightBox(photo, next_id);
         }
     }
 
     function onReady() {
-        lightBox(document.querySelector('.wrapper'));
+        mvtLightBox(document.querySelector('[data-lightbox="mvt-lightbox"]'));
     }
 
     document.addEventListener('DOMContentLoaded', onReady, false);
